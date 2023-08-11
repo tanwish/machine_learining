4ce7b0dc2ab5637a5255957e16d58e2fa3bde268
Tanwish <tanwiish.naiik@gmail.com>
2023-08-11 07:20:13 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-with-MLflow"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "machine_learning"
+AUTHOR_USER_NAME = "tanwish"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "tanwiish.naiik@gmail.com"
 
 
 setuptools.setup(
